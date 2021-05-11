df93e95743cb3373d2dca0958bc7fa63dae29c76
johnsoncls2019 <61136350+johnsoncls2019@users.noreply.github.com>
2021-05-11 22:34:53 +0530

    Add files via upload

---
+++ src/main/java/AchiStarTechnologies/AchiStarTechnologiesApplication.java
@@ -0,0 +1,13 @@
+package AchiStarTechnologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchiStarTechnologiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchiStarTechnologiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AchiStarTechnologies/AchiStarTechnologiesApplicationTests.java
@@ -0,0 +1,13 @@
+package AchiStarTechnologies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AchiStarTechnologiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
